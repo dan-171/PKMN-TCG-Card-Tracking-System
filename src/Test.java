6bb8bf528d9ed6b830aac4a975a3db500461aae0
ann0356 <87ann0506@gmail.com>
2025-06-10 11:05:08 +0800

    lol

--- src/Test.java
+++ src/Test.java
@@ -1,19 +1,13 @@
 
 public class Test {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 	//test classiidfnsf
 	//testing123
-	
-=======
 	//test class （edited on test, trying to merge into main then push)
 	//edited on eclipse
 	//edited on gh
-
 	//added line
 	// blabla
->>>>>>> d0509b0c63939e87b842f4927486f90f09de77f2
-=======
 	//test class
->>>>>>> 40a25f97356595557a30d357e59ddb9a52cd3454
+
 }
