31da7caa8008257cc45f0d6550aa8382c9a8a69c
FBchua <chuafabiean@gmail.com>
2025-06-23 17:43:29 +0800

    update

---
+++ src/Database/CurrentPlayer.java
@@ -0,0 +1,33 @@
+package Database;
+
+public class CurrentPlayer extends Player {
+    private static CurrentPlayer instance = null;
+
+    private CurrentPlayer(int playerID) {
+        super(playerID);
+    }
+
+    // Set the current logged-in player
+    public static void setCurrentPlayer(Player player) {
+        if (player != null) {
+            CurrentPlayer cp = new CurrentPlayer(player.getPlayerID());
+            cp.playerName = player.getPlayerName();
+            cp.cardQuantity = player.getCardQuantity();
+            cp.registerDate = player.getRegistrationDate();
+            cp.password = player.getPassword();
+            instance = cp;
+        }
+    }
+
+    public static CurrentPlayer getCurrentPlayer() {
+        return instance;
+    }
+
+    public static void clearSession() {
+        instance = null;
+    }
+
+    public static boolean isLoggedIn() {
+        return instance != null;
+    }
+}
